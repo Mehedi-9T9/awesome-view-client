152b275dcd87e54fc4f39f4366ac93314e495a24
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-04-29 07:03:30 +0600

    delete work complate

--- src/Components/Pages/MyListCard.jsx
+++ src/Components/Pages/MyListCard.jsx
@@ -2,24 +2,37 @@ import { GiFireplace } from "react-icons/gi";
 import { FaLocationDot } from "react-icons/fa6";
 import { BsPersonRaisedHand } from "react-icons/bs";
 import { SiAmazonsimpleemailservice } from "react-icons/si";
+import { MdDelete } from "react-icons/md";
 
-const MyListCard = () => {
+
+const MyListCard = ({ spot }) => {
+    const { tourismSpotName, tourismCountryName, totalVisitors, location, averageCost, photo
+        , seasonality, _id, userName, currentUser } = spot
+    const deleteHandle = (id) => {
+        fetch(`http://localhost:4000/myList/${id}`, {
+            method: "DELETE",
+            headers: { 'content-type': 'application/json' }
+
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
     return (
 
-        <div className='flex items-center border-b-2  p-2 mx-auto w-[900px]'>
+        <div className='flex items-center border-b-2  p-2 mx-auto '>
             <div className='rounded-xl w-[150px]  '>
-                <img src="https://i.ibb.co/pQSkNYJ/download-8.jpg" className='w-[100px] h-[100px]  rounded-xl ' alt="" />
+                <img src={photo} className='w-[100px] h-[100px]  rounded-xl ' alt="" />
             </div>
             <div className='w-[300px]'>
-                <span className='text-[#E02454] text-2xl font-bold '> <GiFireplace className="inline-block text-3xl" /> Bandarban</span>
-                <h3 className="text-xl font-semibold text-[#002A66]"><FaLocationDot className="inline-block" /> Bangladesh</h3>
+                <span className='text-[#E02454] text-2xl font-bold '> <GiFireplace className="inline-block text-3xl" /> {tourismSpotName}</span>
+                <h3 className="text-xl font-semibold text-[#002A66]"><FaLocationDot className="inline-block" /> {location}</h3>
             </div>
             <div className='w-[250px]'>
-                <span className='text-[#E02454] text-xl font-bold '> <BsPersonRaisedHand className="inline-block text-3xl" /> Mehedi</span>
-                <h3 className="text-[#002A66]  text-xl font-semibold"> <SiAmazonsimpleemailservice className="inline-block text-2xl" />  Email</h3>
+                <span className='text-[#E02454] text-xl font-bold '> <BsPersonRaisedHand className="inline-block text-3xl" /> {userName}</span>
+                <h3 className="text-[#002A66]  text-xl font-semibold"> <SiAmazonsimpleemailservice className="inline-block text-2xl" />  {currentUser}</h3>
             </div>
             <div className='flex flex-col w-[150px] rounded-xl'>
-                <button className='btn bg-[#E02454] text-white font-semibold'>Delete</button>
+                <button onClick={() => deleteHandle(_id)} className='btn bg-[#E02454] text-white font-semibold'><MdDelete className="text-[#002A66] text-2xl" /> Delete</button>
                 {/* <button className='btn '>Details</button> */}
             </div>
         </div>
